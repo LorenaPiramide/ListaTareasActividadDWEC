12ccd28d5566e3f75af1e4a624de3d3eb63c637d
Lorena <230255@fppiramide.com>
2026-02-04 10:14:15 +0100

    Continuación

--- src/constantes.js
+++ src/constantes.js
@@ -2,4 +2,4 @@
 // Obligatorio una única constante URL _SERVER en el que definimos la URL a la que se realizarán las peticiones. 
 // Para la corrección de la actividad se sustituye este valor por el servidor del profesor y debería funcionar sin tener que cambiar nada más.
 
-export const URL_SERVER = "https://"; // Ponerla bien
+export const URL_SERVER = `http://100.49.134.235:3000/`; // Ponerla bien
--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 // Validación del login
+import { URL_SERVER } from './constantes';
+
+document.getElementById("btnLogin").addEventListener("click", function() {
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (!username || !password) {
+        document.getElementById("errorLogin").textContent = "Debes completar todos los campos.";
+        return;
+    }
+
+    fetch(URL_SERVER + "/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username: username,
+            password: password
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+
+        if (data.success) {
+            alert("Login correcto");
+        } else {
+            document.getElementById("errorLogin").textContent = "Campos incorrectos.";
+        }
+    })
+    .catch(error => console.error('Error: ', error));
+});
